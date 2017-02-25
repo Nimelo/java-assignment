9ad57a5e7337b274be80a5200fce3a5fde9fdab2
Mateusz Gąsior <mr.nimelo@gmail.com>
2017-02-25 19:52:52 +0000

    Commented MainController funcitonalities

--- code/src/main/java/controllers/MainController.java
+++ code/src/main/java/controllers/MainController.java
@@ -33,13 +33,14 @@ public class MainController {
 
     /**
      * Routine that calculates solution of problem Ax = b
+     *
      * @return Result of LU Pivot routine.
-     * @throws NonSquareMatrixException
-     * @throws ZeroPivotException
-     * @throws InvalidMatrixSizeForMultiplication
-     * @throws InvalidMatrixSizesException
-     * @throws LUPivotConstraintsException
-     * @throws MatrixVectorMultiplicationSizeException
+     * @throws NonSquareMatrixException                when matrix does not have a square shape.
+     * @throws ZeroPivotException                      when zero occurs in pivot.
+     * @throws InvalidMatrixSizeForMultiplication      when there will be invalid size of matrices during multiplication.
+     * @throws InvalidMatrixSizesException             when matrices during calculations will have different sizes.
+     * @throws LUPivotConstraintsException             when requirements for performing LU pivoting will not be meet.
+     * @throws MatrixVectorMultiplicationSizeException when size of vector and size of matrix are not correct for multiplication.
      */
     public LUPivotResult LUPivot() throws NonSquareMatrixException, ZeroPivotException, InvalidMatrixSizeForMultiplication, InvalidMatrixSizesException, LUPivotConstraintsException, MatrixVectorMultiplicationSizeException {
         checkLUPivotConstraints();
@@ -51,12 +52,22 @@ public class MainController {
         return new LUPivotResult(matrix, vector, factorize.getL(), factorize.getU(), solution, determinant);
     }
 
+    /**
+     * Checks if inverse routine can be performed.
+     *
+     * @throws InversionConstraintsException when routine cannot be performed.
+     */
     private void checkInverseConstraints() throws InversionConstraintsException {
         if (matrix == null || !matrix.isSquare()) {
             throw new InversionConstraintsException();
         }
     }
 
+    /**
+     * Checks if LU Pivoting routine can be performed.
+     *
+     * @throws LUPivotConstraintsException when routine cannot be performed.
+     */
     private void checkLUPivotConstraints() throws LUPivotConstraintsException {
         if (matrix == null || vector == null) {
             throw new LUPivotConstraintsException();
@@ -67,6 +78,12 @@ public class MainController {
         }
     }
 
+    /**
+     * Inverse routine.
+     *
+     * @return Result of inverse routine for view.
+     * @throws InversionConstraintsException when requirements for inverse routine does not meet desired requirements.
+     */
     public InverseResult inverse() throws InversionConstraintsException {
         checkInverseConstraints();
         Matrix inverse = null;
@@ -95,6 +112,14 @@ public class MainController {
         }
     }
 
+    /**
+     * Extracts and sets matrix from a String.
+     * Column separator is space.
+     * Row separator is a new line character.
+     *
+     * @param matrix Matrix in a string format.
+     * @throws NotEqualAmountOfColumnsInMatrixException when number of columns is different for rows.
+     */
     public void extractAndSetMatrix(String matrix) throws NotEqualAmountOfColumnsInMatrixException {
         final String rowsSeparator = "\n";
         final String columnSeparator = " ";
@@ -116,6 +141,12 @@ public class MainController {
         this.matrix = new Matrix(data);
     }
 
+    /**
+     * Extracts and sets vector from a String.
+     * Value separator is single space.
+     *
+     * @param vector Vector in a string format.
+     */
     public void extractAndSetVector(String vector) {
         final String valuesSeparator = " ";
 
